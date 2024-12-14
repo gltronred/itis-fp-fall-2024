68db23d2480bac60598b4ea5f67e9530a271b05a
Mansur Ziiatdinov <gltronred@gmail.com>
2024-12-14 10:56:22 +0300

    Using TVar

--- lessons/src/D6.hs
+++ lessons/src/D6.hs
@@ -3,15 +3,30 @@
 module D6 where
 
 import Control.Monad
+import System.Random
+
 import Control.Concurrent
 import Control.Concurrent.STM
 
-thread d k = do
+thread :: TVar Int -> Int -> IO ()
+thread v k = do
   putStrLn $ "Hello from Thread " ++ show k
+  d <- randomRIO (1_000_000, 3_000_000)
   threadDelay d
+  r <- randomRIO (-1000, 1000)
+  atomically $ do
+    -- не сработает: randomRIO работает в IO!
+    -- r <- randomRIO (-1000, 1000)
+    modifyTVar' v (+r)
   putStrLn $ "Bye from Thread " ++ show k
 
+run :: IO ()
 run = do
+  v <- newTVarIO 0
   forM_ [1..3] $ \k -> do
-    forkIO $ thread 3000000 k
-  thread 4000000 0
+    forkIO $ thread v k
+  x <- atomically $ readTVar v
+  print x
+  threadDelay 4_000_000
+  y <- atomically $ readTVar v
+  print y
